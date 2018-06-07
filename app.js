dd4683799e2d7b7aa4480b5e5656aa7d881d8fa5
PloyNattawadee <ploy14794@hotmail.com>
2018-06-07 21:10:41 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -2,49 +2,59 @@ const express = require('express');
 const mysql = require('mysql');
 const Register = require('./api/model/db')
 const route = require('./api/controller/register')
-var bodyParser = require('body-parser');
-var cors = require('cors');
+const routecategory = require('./api/controller/category')
+const routeproduct = require('./api/controller/product')
+const routeImg = require('./api/controller/productimage')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const http = require('http');
-var url = require('url');
-var path = require('path')
+const url = require('url');
+const path = require('path')
 const morgan = require('morgan');
-var querystring = require('querystring');
-var cookieParser = require('cookie-parser');
+const querystring = require('querystring');
+const cookieParser = require('cookie-parser');
+const cookieHelper = require('./api/controller/cookies-helper')
 
 const app = express();
 
 http.createServer(function (req, res ,next) {
     console.log(method.req)
    // var cookies = parserCookies(req);
+   console.log("Header : "+headers)
     const { headers, method, url } = req;
     let body = [];
 
     res.setHeader('Content-Type', 'application/json');
-
+    
     req.on('error', (err) => {
         console.log(err);
     }).on('data', (user) => {
-        body += user
+        //body += user
+        body.push(user)
     }).on('end', () => {
-        var post = querystring.parse(body);
+        let post = querystring.parse(body);
         const responseBody = { headers, method, url, body };
         res.write("Successfull");
         console.log("ploy" + body);
         console.log('SDSAADS2 ' + JSON.stringify(post));
 
-        res.setHeader('Set-Cookie', 'mycookies = cookies')
         res.end();
 
     });
-    next();
+
 })
 app.listen('3000', () => {
     console.log('Server started on port 3000');
 });
 
 app.use(cookieParser());
+route.use(cookieParser());
 
 route.use(cors())
+routeproduct.use(cors())
+routeImg.use(cors())
+routecategory.use(cors())
+//cookieHelper.use(cors())
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -53,10 +63,14 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
     next();
 });
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true }))
+app.use(bodyParser.json({limit: '50mb'}))
 app.use(morgan('dev'))
 
 app.use('/account', route)
+app.use('/category',routecategory)
+app.use('/product',routeproduct)
+app.use('/image',routeImg)
+
 
 
